152146a7a3ec4e64d050b420c382fc64007ddb18
fbormann <felipe.bormann@gmail.com>
2016-09-14 17:48:45 -0300

    teste

--- calc.py
+++ calc.py
@@ -1,86 +1,97 @@
 INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
 
 class Token(object):
-	def __init__(self, type, value):
-		# token type: Integer, plus, eof
-		self.type = type
-		# token value : 1,2,3,4...'+'
-		self.value = value
+    def __init__(self, type, value):
+        # token type: Integer, plus, eof
+        self.type = type
+        # token value : 1,2,3,4...'+'
+        self.value = value
 
-	def __str__(self):
-		return 'Token {type} {value}'.format(type=self.type,
-			value=repr(self.value))
+    def __str__(self):
+        return 'Token({type} {value})'.format(type=self.type,
+            value=repr(self.value))
 
-	def repr(self):
-		return self.__str__()
+    def __repr__(self):
+        return self.__str__()
 
 
 class Interpreter(object):
-	def __init__(self, text):
-		self.text = text
-		self.pos = 0
-		self.current_token = None
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+        self.current_char = self.text[self.pos]
 
-	def error(self):
-		raise Exception('Error parsing input')
+    def error(self):
+        raise Exception('Error parsing input')
 
-	def get_next_token(self):
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
 
-		text = self.text
-		if self.pos > len(text) - 1:
-			return Token(EOF, None)
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
 
-		current_char = text[self.pos]
-		if current_char.isdigit():
-			digit = int(current_char)
-			
-			number = digit
-			if self.pos < len(text) - 1:
-				self.pos += 1
-				current_char = text[self.pos]
-				while current_char.isdigit(): #this add support for multiple digit integers
-					number *= 10
-					digit = int(current_char)
-					number += digit
-					self.pos += 1
-					if self.pos > len(text) - 1:
-						break
-					current_char = text[self.pos]
-			token = Token(INTEGER, number)
-			print(token.repr())
-			return token
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
 
-		if current_char == '+':
-			token = Token(PLUS, current_char)
-			self.pos += 1
-			return token
 
-		self.error()
-	
-	def eat(self, token_type):
-	# compare the current token type with the passed token
+    def get_next_token(self):
+
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            self.error()
+
+        return Token(EOF, None)
+    
+    def eat(self, token_type):
+        # compare the current token type with the passed token
         # type and if they match then "eat" the current token
         # and assign the next token to the self.current_token,
         # otherwise raise an exception.
-		if self.current_token.type == token_type:
-			self.current_token = self.get_next_token()
-		else:
-            		self.error()
-	def expr(self):
-		"""expr -> INTEGER PLUS INTEGER"""
-		# set current token to the first token taken from the input
-		self.current_token = self.get_next_token()
-		# we expect the current token to be a single-digit integer
-		left = self.current_token
-		self.eat(INTEGER)
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        """expr -> INTEGER PLUS INTEGER"""
+        # set current token to the first token taken from the input
+        self.current_token = self.get_next_token()
+        # we expect the current token to be a single-digit integer
+        left = self.current_token
+        self.eat(INTEGER)
 
         # we expect the current token to be a '+' token
-		op = self.current_token
-		self.eat(PLUS)
+        op = self.current_token
+        if op.type == PLUS:
+            self.eat(PLUS)
+        else:
+            self.eat(MINUS)
 
         # we expect the current token to be a single-digit integer
-		right = self.current_token
-		self.eat(INTEGER)
+        right = self.current_token
+        self.eat(INTEGER)
         # after the above call the self.current_token is set to
         # EOF token
 
@@ -88,8 +99,11 @@ class Interpreter(object):
         # has been successfully found and the method can just
         # return the result of adding two integers, thus
         # effectively interpreting client input
-		result = left.value + right.value
-		return result
+        if op.type == PLUS:
+            result = left.value + right.value
+        else:
+            result = left.value - right.value
+        return result
 
 
 def main():
@@ -102,6 +116,8 @@ def main():
             break
         if not text:
             continue
+        
+        print(text)
         interpreter = Interpreter(text)
         result = interpreter.expr()
         print(result)
